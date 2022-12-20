348e619832ad6fbeb6110587f4b43c8385028621
lowqwroby <eltiobvs@gmail.com>
2022-12-20 20:40:15 +0300

    Добавьте файлы проекта.

---
+++ Task_9_1_Forms_PM01_N3/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_9_1_Forms_PM01_N3
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				string[] temp = textBox1.Text.Split(' ');
+				double[] numbers = new double[temp.Length];
+
+				for(int i = 0; i < temp.Length; i++)
+				{
+					numbers[i] = double.Parse(temp[i]);
+				}
+
+				FileStream f = new FileStream("t.dat", FileMode.Open);
+				BinaryWriter fOut = new BinaryWriter(f);
+
+				for (int i = 0; i < numbers.Length; i++)
+				{
+					fOut.Write(numbers[i]);
+				}
+
+				fOut.Close();
+				f = new FileStream("t.dat", FileMode.OpenOrCreate);
+				BinaryReader fIn = new BinaryReader(f);
+				long m = f.Length;
+
+				double num = double.Parse(textBox2.Text);
+
+				using (StreamWriter sw = new StreamWriter(File.Open("t.txt", FileMode.Create)))
+				{
+					for (long i = 0; i < m; i += 16)
+					{
+						f.Seek(i, SeekOrigin.Begin);
+						double a = fIn.ReadDouble();
+						if (a <= num)
+						{
+							textBox3.Text += string.Format(a + " ");
+							sw.Write(a + " ");
+						}
+					}
+				}
+
+				fIn.Close();
+				f.Close();
+			}
+			catch (FormatException)
+			{
+				MessageBox.Show("Введены неверные параметры!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+			catch
+			{
+				MessageBox.Show("Неизвестная ошибка!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+	}
+}
---
+++ Task_9_2_Forms_PM01_N3/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_9_2_Forms_PM01_N3
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				textBox1.Text = "";
+				StreamReader fileIn = new StreamReader("text.txt");
+				string text = fileIn.ReadToEnd();
+				fileIn.Close();
+				string[] newText = Regex.Split(text, "[\n]+");
+				
+				for(int i = 0; i < newText.Length; i++)
+				{
+					for(int j = 0; j < newText[i].Length; j++)
+					{
+						if (newText[i][j] == ' ')
+						{
+							textBox1.Text += string.Format(newText[i] + "\n");
+							break;
+						}
+					}
+				}
+			}
+			catch
+			{
+				textBox2.Text = string.Format("Ошибка. Вероятно, указанный файл не найден.");
+			}
+		}
+	}
+}
